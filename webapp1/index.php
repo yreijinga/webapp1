9ae5fbb36039b74ca4aab0e82f72e46a65cc3b38
Youri Reijinga <1201111@student.roc-nijmegen.nl>
2023-03-21 11:26:54 +0100

    first commit

---
+++ webapp1/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP wow</h1>
+    <div>
+        <?php
+        
+        /* Connect to a MySQL database using driver invocation */
+        $dsn = 'mysql:dbname=webapplicatie;host=127.0.0.1';
+        $user = 'root';
+        $password = '';
+
+        try {
+            $connectie = new PDO($dsn, $user, $password);
+            echo "Verbinding gelukt";
+        } catch (PDOException $e) {
+            echo "Verbinding niet gelukt: " . $e;
+        }
+
+        $resultSet = $connectie->query("SELECT * FROM `menu`");
+
+        /* een voor een
+        $menuItem = $resultSet->fetch();
+        var_dump($menuItem);
+        */
+
+        while ($item = $resultSet->fetch()) {
+            echo "<div>
+            <h4>" . $item['titel'] . "</h2>
+            <p>" . $item['omschrijving'] . "</p>
+            <p>" . $item['prijs'] . "</p>
+            </div>";
+        }
+
+        ?>
+        <h1>strong army wow</h1>
+    </div>
+</body>
+</html>
---
+++ webapp1/menusql.sql
@@ -0,0 +1,4 @@
+USE webapplicatie;
+SELECT * FROM `menu`;
+
+INSERT INTO `menu`(`titel`, `omschrijving`, `prijs`) VALUES ('Tosti kaas', 'Een warme tosti met kaas', '2,5')
