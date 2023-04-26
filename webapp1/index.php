818c8f713e0a40ed27e3e6280e57ae2b1fe8e9c2
Youri Reijinga <1201111@student.roc-nijmegen.nl>
2023-04-26 11:23:29 +0200

    test commit

--- webapp1/index.php
+++ webapp1/index.php
@@ -1,14 +1,10 @@
-<?php
-session_start()
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=stylesheet href="css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>webapp 1 youri</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,7 +13,9 @@ session_start()
 <body>
     <nav>
         <div class="nav-header">
-            <h1>Restaurant met eten</h1>
+            <div class="main-title">
+                <h1>Lunchrestaurant</h1>
+            </div>
         </div>
     </nav>
     <h1></h1>
